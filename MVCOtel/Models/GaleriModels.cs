f6025b36ac3db0be2291946319d624db9f1cb356
section-1 <section-1@DESKTOP-93G7NTJ>
2016-11-21 10:33:30 +0300

    Add project files.

---
+++ MVCOtel/Models/GaleriModels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCOtel.Models
+{
+    public class GaleriModels
+    {
+        [Key]
+        public int GaleriID { get; set; }
+        public string Yazi { get; set; }
+        public string ResimURL { get; set; }
+    }
+}
---
+++ MVCOtel/Models/RezervasyonM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCOtel.Models
+{
+   public  class RezervasyonM
+    {
+        [Key]
+        public int RezervasyonMID { get; set; }
+        public string UserID { get; set; }
+        public int OdaID { get; set; }
+        public DateTime GirisTarih { get; set; }
+        public DateTime CıkısTarih { get; set; }
+        public decimal Fiyat { get; set; }
+        public int KacKisi { get; set; }
+
+    }
+}
